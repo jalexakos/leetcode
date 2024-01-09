21627f658c4c23289091664217989f2988a09e40
Josiah Alexakos <jalexakos@linkedin.com>
2024-01-08 16:59:10 -0800

    Second attempt at search insert position; got it to work

--- easy/35-search-insert-position.ts
+++ easy/35-search-insert-position.ts
@@ -1,24 +1,43 @@
 // from https://leetcode.com/problems/search-insert-position/
 
+// function searchInsert(nums: number[], target: number): number {
+//     let leftPointer = 0;
+//     let rightPointer = nums.length - 1;
+//     let currentIdx = 0;
+
+//     while (leftPointer < rightPointer){
+//         currentIdx = Math.floor((rightPointer + leftPointer) / 2);
+
+//         if (nums[currentIdx] === target) return currentIdx;
+//         else if ((leftPointer + 1 === rightPointer) && target > nums[leftPointer] && target < nums[rightPointer]){
+//             return leftPointer + 1;
+//         }
+//         else if (nums[currentIdx] < target){
+//             leftPointer = currentIdx + 1;
+//         } else {
+//             rightPointer = currentIdx - 1;
+//         }
+//     }
+
+//     if (target < nums[leftPointer]) return leftPointer;
+//     else return rightPointer;
+// };
+
 function searchInsert(nums: number[], target: number): number {
     let leftPointer = 0;
     let rightPointer = nums.length - 1;
     let currentIdx = 0;
 
-    while (leftPointer < rightPointer){
+    while (leftPointer <= rightPointer){
         currentIdx = Math.floor((rightPointer + leftPointer) / 2);
 
         if (nums[currentIdx] === target) return currentIdx;
-        else if ((leftPointer + 1 === rightPointer) && target > nums[leftPointer] && target < nums[rightPointer]){
-            return leftPointer + 1;
-        }
         else if (nums[currentIdx] < target){
             leftPointer = currentIdx + 1;
         } else {
             rightPointer = currentIdx - 1;
         }
     }
-
-    if (target < nums[leftPointer]) return leftPointer;
-    else return rightPointer;
+    
+    return leftPointer;
 };
