8784159748ef68091c089f146f097a532c719777
Josiah Alexakos <jalexakos@linkedin.com>
2024-01-31 08:38:56 -0800

    Solution for palindrome linked list

---
+++ easy/234-palindrome-linked-list.ts
@@ -0,0 +1,24 @@
+// from https://leetcode.com/problems/palindrome-linked-list/description/
+
+// Definition for singly-linked list can be found in easy/206-reverse-linked-list.ts
+
+function isPalindrome(head: ListNode | null): boolean {
+    let list = [];
+    while (head) {
+        list.push(head.val);
+        head = head.next;
+    }
+
+    let x = 0;
+    let y = list.length - 1;
+    while (x <= y){
+        if (list[x] !== list[y]) return false;
+        x++;
+        y--;
+    }
+
+    return true;
+};
+
+// runtime 17th pecentile
+// memory 60th percentile
