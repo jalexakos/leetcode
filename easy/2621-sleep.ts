b344f7303e3e0688d781f7c5f79f2f77eefe86f2
Josiah Alexakos <jalexakos@linkedin.com>
2024-02-24 10:42:39 -0800

    Add link and data

--- easy/2621-sleep.ts
+++ easy/2621-sleep.ts
@@ -1,3 +1,5 @@
+// from https://leetcode.com/problems/sleep/
+
 async function sleep(millis: number): Promise<void> {
     return new Promise(resolve => {
       setTimeout(resolve, millis);
@@ -9,3 +11,6 @@ async function sleep(millis: number): Promise<void> {
    * let t = Date.now()
    * sleep(100).then(() => console.log(Date.now() - t)) // 100
    */
+
+// runtime 32nd percentile
+// memory 30th percentile
