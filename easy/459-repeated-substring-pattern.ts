a683a337f442d71f6a6e2d8bf2b0a1045d1f1593
Josiah Alexakos <jalexakos@linkedin.com>
2024-03-06 14:16:12 -0800

    Optimize repeated substrings pattern solution

--- easy/459-repeated-substring-pattern.ts
+++ easy/459-repeated-substring-pattern.ts
@@ -20,3 +20,32 @@ function repeatedSubstringPattern(s: string): boolean {
 
 // runtime 37th percentile
 // memory 37th percentile
+
+// I thought about this more, and thought I could optimize it 
+// for memory (and maybe runtime). I was surprised with runtime 
+// improvements, though memory did not improve much.
+function repeatedSubstringPatternOpt(s: string): boolean {
+    if (s.length === 1) return false;
+    const sLength = s.length;
+    for (let i = 1; i < (sLength / 2) + 1; i++) {
+        ifCheck: if ((sLength % i) === 0) {
+            let subStr = s.slice(0,i);
+            let subLength = subStr.length;
+            let start = subLength;
+            let end = subLength * 2;
+            while (end <= sLength) {
+                if (subStr !== s.slice(start, end)){
+                    break ifCheck;
+                }
+                start += subLength;
+                end += subLength;
+            }
+            return true;
+        }
+    }
+
+    return false;
+};
+
+// runtime 96th percentile
+// memory 46th percentile
