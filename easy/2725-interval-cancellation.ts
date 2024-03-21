19799a4d824bb6a7267d0afb7a89ce1e054f5dac
Josiah Alexakos <jalexakos@linkedin.com>
2024-03-21 11:53:32 -0700

    Solution for interval cancellation

---
+++ easy/2725-interval-cancellation.ts
@@ -0,0 +1,45 @@
+// from https://leetcode.com/problems/interval-cancellation/
+
+type JSONValueIntervalCancel = null | boolean | number | string | JSONValueIntervalCancel[] | { [key: string]: JSONValueIntervalCancel };
+type FnIntervalCancel = (...args: JSONValueIntervalCancel[]) => void
+
+function cancellable(fn: FnIntervalCancel, args: JSONValueIntervalCancel[], t: number): Function {
+    fn(...args);
+    let interval = setInterval(() => fn(...args), t);;
+
+    return function () {
+        clearInterval(interval);
+    }
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 2;
+ *  const args = [4], t = 35, cancelTimeMs = 190;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *   
+ *  setTimeout(() => {
+ *      console.log(result); // [
+ *                           //     {"time":0,"returned":8},
+ *                           //     {"time":35,"returned":8},
+ *                           //     {"time":70,"returned":8},
+ *                           //     {"time":105,"returned":8},
+ *                           //     {"time":140,"returned":8},
+ *                           //     {"time":175,"returned":8}
+ *                           // ]
+ *  }, cancelTimeMs + t + 15)    
+ */
+
+// runtime 75th percentile
+// memory 51st percentile
