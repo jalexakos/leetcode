fbb0bf8d55a200bfed166cd3d5aa3266f35516b8
Josiah Alexakos <jalexakos@linkedin.com>
2024-04-21 19:02:32 -0700

    Solution for To Lower Case

---
+++ easy/709-to-lower-case.ts
@@ -0,0 +1,8 @@
+// from https://leetcode.com/problems/to-lower-case/
+
+function toLowerCase(s: string): string {
+    return s.toLowerCase();
+};
+
+// runtime 32nd percentile
+// memory 96th percentile
