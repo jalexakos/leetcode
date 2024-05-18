b6e4f0af5a49ecc53a56ca91b13f580cc98dff56
Josiah Alexakos <jalexakos@linkedin.com>
2024-05-18 15:36:44 -0700

    Solution for Distribute Candies to People

---
+++ easy/1103-distribute-candies-to-people.ts
@@ -0,0 +1,27 @@
+// from https://leetcode.com/problems/distribute-candies-to-people/description/
+
+function distributeCandies(candies: number, num_people: number): number[] {
+    let arr = Array(num_people).fill(0);
+
+    let candy = 1;
+    let i = 0;
+    while (candies > 0) {
+        if (candies < candy) {
+            candy = candies;
+            candies = 0;
+        }
+        if (i > arr.length - 1) {
+            i = 0;
+        }
+         
+        arr[i] = arr[i] + candy;
+        i++;
+        candies -= candy;
+        candy++;
+    }
+
+    return arr;
+};
+
+// runtime 100th percentile
+// memory 50th percentile
