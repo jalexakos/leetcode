ce75ac0e3765ea7e33820abc9648a43f7a0502c4
Josh Alexakos <josiah.alexakos@gmail.com>
2024-05-31 14:24:50 -0700

    Solution for Kth Largest Element in a Stream (part 2)

--- easy/703-kth-largest-element-in-a-stream.ts
+++ easy/703-kth-largest-element-in-a-stream.ts
@@ -21,3 +21,29 @@ class KthLargest {
 
 // runtime 13th percentile
 // memory 5th percentile
+
+// re-doing this with a slight optimization
+class KthLargestOpt {
+    constructor(private k: number, private nums: number[]) {
+        this.k = k;
+        this.nums = nums.sort((a,b) => a - b);
+    }
+
+    add(val: number): number {
+        let i = 0;
+        while (val > this.nums[i]) {
+            i++;
+        }
+        this.nums.splice(i,0,val);
+        return this.nums[this.nums.length - this.k];
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
+
+// runtime 30th percentile
+// memory 93rd percentile
