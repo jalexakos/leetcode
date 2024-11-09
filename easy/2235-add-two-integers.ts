85c84c8343e279a193b0df81e724a9f6f2a8ddfc
Josh Alexakos <64025205+jalexakos@users.noreply.github.com>
2024-11-09 09:12:40 -0800

    Solution for Add Two Integers (#312)

---
+++ easy/2235-add-two-integers.ts
@@ -0,0 +1,9 @@
+// Add Two Integers
+// https://leetcode.com/problems/add-two-integers/description/
+
+function sum(num1: number, num2: number): number {
+    return num1 + num2;
+};
+
+// runtime 100th percentile
+// memory 78th percentile
